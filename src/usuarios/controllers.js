c53c95519a471732f9b738f69faa183223453504
laumdo <lmarti42@ucm.es>
2025-03-05 10:33:05 +0300

    Usuarios BD

--- src/usuarios/controllers.js
+++ src/usuarios/controllers.js
@@ -1,35 +1,60 @@
 import { body, validationResult } from 'express-validator';
+//import { Usuario, RolesEnum } from './Usuario.js';
 
 export function viewLogin(req, res) {
-    res.render('pagina', {contenido: 'paginas/login', session: req.session, error: null});
+    let contenido = 'paginas/login';
+    if (req.session != null && req.session.login) {
+        contenido = 'paginas/index'
+    }
+    res.render('pagina', {
+        contenido,
+        session: req.session
+    });
 }
 
 export function doLogin(req, res) {
     body('username').escape(); // Se asegura que eliminar caracteres problemáticos
     body('password').escape(); // Se asegura que eliminar caracteres problemáticos
 
-    const {username, password} = req.body;
-    if(username === 'user' && password === 'userpass'){
-        req.session.login = true;
-        req.session.nombre = 'Usuario';
-    }else if(username === 'admin' && password === 'adminpass'){
-        req.session.login = true;
-        req.session.nombre = 'Administrador';
-        req.session.esAdmin = true;
-    }else{
-        return res.render('pagina', {contenido: 'paginas/login', session: req.session, error: 'Nombre o contraseña incorrecta'});
-    }
+    // Capturo las variables username y password
+    const username = req.body.username.trim();
+    const password = req.body.password.trim();
 
-    return res.render('pagina', {contenido: 'paginas/bienvenida', session: req.session});
+    try {
+        const usuario = Usuario.login(username, password);
+        req.session.login = true;
+        req.session.nombre = usuario.nombre;
+        req.session.esAdmin = usuario.rol === RolesEnum.ADMIN;
+
+        return res.render('pagina', {
+            contenido: 'paginas/index',
+            session: req.session
+        });
+
+    } catch (e) {
+        res.render('pagina', {
+            contenido: 'paginas/login',
+            error: 'El usuario o contraseña no son válidos'
+        })
+    }
 }
 
 export function doLogout(req, res, next) {
     // TODO: https://expressjs.com/en/resources/middleware/session.html
-    delete req.session.login;
-    delete req.session.nombre;
-    if(req.session.esAdmin){
-        delete req.session.esAdmin;
-    }
+    // clear the user from the session object and save.
+    // this will ensure that re-using the old session id
+    // does not have a logged in user
+    req.session.login = null
+    req.session.nombre = null;
+    req.session.esAdmin = null;
+    req.session.save((err) => {
+        if (err) next(err);
 
-    res.render('pagina', {contenido: 'paginas/logout', mensaje: 'Gracias por visitar nuestra web. Hasta pronto.'});
+        // regenerate the session, which is good practice to help
+        // guard against forms of session fixation
+        req.session.regenerate((err) => {
+            if (err) next(err)
+            res.redirect('/');
+        })
+    })
 }
