bfcaf5fc4a36d72165e159bff61032e9df5a2b71
aligrado <aligrado@ucm.es>
2025-05-13 00:45:03 +0200

    arreglos

--- src/entradasUsuario/EntradasUsuario.js
+++ src/entradasUsuario/EntradasUsuario.js
@@ -10,7 +10,7 @@ export class EntradasUsuario {
     static initStatements(){
         const db = getConnection();
         
-        this.#insertStmt = db.prepare('INSERT INTO entradasUsuario(idUsuario, idEvento, cantidad, fecha_compra) VALUES (@id_usuario, @id_evento, @cantidad, @fecha_compra)');
+        this.#insertStmt = db.prepare('INSERT INTO entradasUsuario(idUsuario, idEvento, cantidad) VALUES (@id_usuario, @id_evento, @cantidad)');
         this.#updateStmt = db.prepare('UPDATE entradasUsuario SET cantidad = cantidad + @cantidad WHERE idUsuario = @id_usuario AND idEvento = @id_evento');
         this.#checkStmt = db.prepare('SELECT COUNT(*) as count FROM entradasUsuario WHERE idUsuario = @id_usuario AND idEvento = @id_evento');
         this.#getAllStmt = db.prepare('SELECT * FROM entradasUsuario WHERE idUsuario = @id_usuario');
@@ -21,31 +21,31 @@ export class EntradasUsuario {
         return rows.map(row => new EntradasUsuario(row));
     }
 
-    static #insert(id_usuario, id_evento, cantidad,fecha_compra){
+    static #insert(id_usuario, id_evento, cantidad){
         let result = null;
         try{
-            result = this.#insertStmt.run({id_usuario, id_evento, cantidad,fecha_compra});
+            result = this.#insertStmt.run({id_usuario, id_evento, cantidad});
         }catch(e){
             throw new ErrorDatos('No se ha podido proceder a la compra', { cause: e});
         }
         return result;
     }
 
-    static #update(id_usuario, id_evento, cantidad,fecha_compra){
-        const result = this.#updateStmt.run({id_usuario, id_evento, cantidad,fecha_compra});
+    static #update(id_usuario, id_evento, cantidad){
+        const result = this.#updateStmt.run({id_usuario, id_evento, cantidad});
 
         if(result.changer === 0) throw new ErrorDatos('Error');
         return result;
     }
 
-    static compraEntrada(id_usuario, id_evento, cantidad,fecha_compra) {
-        const existe = this.#checkStmt.get({ id_usuario, id_evento,fecha_compra });
+    static compraEntrada(id_usuario, id_evento, cantidad) {
+        const existe = this.#checkStmt.get({ id_usuario, id_evento });
         console.log('existe:', existe.count);
     
         if (existe.count === 0) {
-            this.#insert(id_usuario, id_evento, cantidad,fecha_compra);
+            this.#insert(id_usuario, id_evento, cantidad);
         } else {
-            this.#update(id_usuario, id_evento, cantidad,fecha_compra);
+            this.#update(id_usuario, id_evento, cantidad);
         }
     }
 
@@ -53,16 +53,12 @@ export class EntradasUsuario {
     idUsuario;
     idEvento;
     cantidad;
-    fecha_compra;
 
-    //constructor(idUsuario, idEvento, cantidad,fecha_compra){ EXPIRACION DE PUNTOS
-     //   this.#id = null;
     constructor({id = null, idUsuario, idEvento, cantidad}){
         this.#id = id;
         this.idUsuario = idUsuario;
         this.idEvento = idEvento;
         this.cantidad = cantidad;
-        this.fecha_compra=fecha_compra;
     }
 
     persist() {
